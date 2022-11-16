a6820c67ed1f9401a600581235737c64a23697e5
Leandro Casuso Montero <casuso.montero@gmail.com>
2022-11-16 10:21:54 -0500

    Cli parameters shorts

--- src/cli.rs
+++ src/cli.rs
@@ -1,35 +1,36 @@
 use clap::{Parser};
 
+//ToDo
+// Know how to deal with parameter with the '_'
+// Think in better names and/or shorts.
+// Parameter height default value explanation.
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
 
     /// [Optional] Path to config file.
     /// If this value is not passed the default config will be used.
-    #[clap(short, long)]
+    #[clap(short = 'c', long)]
     pub config_path: Option<String>,
 
-    /// [Optional] Starts refiner from specified height.
+    /// [Optional] Starts refiner at the specified height.
     /// If this value is not passed, first height will be used.
-    #[clap(short = 'n', long)]
+    #[clap(short = 's', long)]
     pub near_block_height_start: Option<u64>,
 
-    /// [Optional] Ends refiner in the specified height.
+    /// [Optional] Ends refiner at the specified height.
     /// If this value is not passed, last height will be used.
-    #[clap(short = 'n', long)]
+    #[clap(short = 'e', long)]
     pub near_block_height_end: Option<u64>,
 
     /// [Optional] Near block expression to match.
-    /// If this value is not passed it will not be used to find matches.
-    #[clap(short, long)]
+    /// If this value is not passed, it will not be used to find matches.
+    #[clap(short = 'n', long)]
     pub near_block_expression: Option<String>,
 
     /// [Optional] Aurora block expression to match.
-    /// If this value is not passed it will not be used to find matches.
-    #[clap(short, long)]
+    /// If this value is not passed, it will not be used to find matches.
+    #[clap(short = 'a', long)]
     pub aurora_block_expression: Option<String>,
-
-    // ToDo:
-    // Not sure about the 'short' and 'long' parameters here.
-    // Parameter height default value explanation.
 }
